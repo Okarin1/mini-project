e98e0419bb847574af4fb27369bde7bf73650531
Retr0 <wang.zhou@hand-china.com>
2022-05-08 01:03:35 +0800

    update: 工具函数更新

--- utils/helper.js
+++ utils/helper.js
@@ -9,8 +9,23 @@ export function queryRect(selector) {
   })
 }
 
+
+export function sortItemByDate(posts) {
+  posts.sort((prev, next) => {
+    return compareDate(prev, next)
+  })
+}
+
+// 比对时间
+export function compareDate(a, b) {
+  const aDateNum = getTimeNum(a.createDate)
+  const bDateNum = getTimeNum(b.createDate)
+  if (aDateNum === 0 && bDateNum === 0) return 0
+  return bDateNum - aDateNum
+}
+
 // 获取时间的数字类型
-export function getTimeNum (date) {
-const dateNum = !date ? 0 : new Date(date).getTime()
-return dateNum
+export function getTimeNum(date) {
+  const dateNum = !date ? 0 : new Date(date).getTime()
+  return dateNum
 }
