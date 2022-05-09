6b85650e1992245caa80bd3ca1c09f392ea33df8
Retr0 <wang.zhou@hand-china.com>
2022-05-09 23:26:26 +0800

    fix: 修复了从通知进入的帖子详情无头像和昵称的bug

--- pages/post-detail/index.js
+++ pages/post-detail/index.js
@@ -1,48 +1,61 @@
 // pages/post-detail/index.js
-import{ getPostByPostId ,getPostThumbsList,getPostCommentList} from"../../service/api_home"
+import {
+  getPostByPostId,
+  getPostThumbsList,
+  getPostCommentList
+} from "../../service/api_home"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  postInfo:{},
-  active: 1,
-  thumbsList:[],
-  commentList:[]
+    postInfo: {},
+    active: 1,
+    thumbsList: [],
+    commentList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    if(options.postInfo){
+    if (options.postInfo) {
       let postInfo = JSON.parse(options.postInfo)
       this.setData({
-        postInfo:postInfo
+        postInfo: postInfo
       })
-    }else{
-      if(options.postsId){
-        getPostByPostId(options.postsId).then(res=>{
-          this.setData({
-            postInfo:res.posts
-          })
-        })
-      }
+      this.getPostDetail(postInfo.postsId)
     }
-    let postsId = this.data.postInfo.postsId
-    if(postsId){
-      getPostThumbsList(postsId).then(res=>{
-        if(res.msg == "success"){
+    if (options.postsId) {
+      getPostByPostId(options.postsId).then(res => {
+        let postInfo = res.posts
+        postInfo.username = res.user[0].username
+        postInfo.nickname = res.user[0].nickname
+        postInfo.headPortrait = res.user[0].headPortrait
+        this.setData({
+          postInfo: postInfo
+        })
+        this.getPostDetail(options.postsId)
+      })
+    }
+
+   
+  },
+
+  getPostDetail(postsId){
+    if (postsId) {
+      getPostThumbsList(postsId).then(res => {
+        if (res.msg == "success") {
           this.setData({
-            thumbsList:res.records
+            thumbsList: res.records
           })
         }
       })
-      getPostCommentList(postsId,1).then(res=>{
-        if(res.msg == "success"){
+      getPostCommentList(postsId, 1).then(res => {
+        if (res.msg == "success") {
           this.setData({
-            commentList:res.Commentlist.records
+            commentList: res.Commentlist.records
           })
         }
       })
@@ -55,13 +68,13 @@ Page({
     //   icon: 'none',
     // });
   },
-  showUserInfo(){
+  showUserInfo() {
     let username = this.data.postInfo.username
     wx.navigateTo({
       url: '/pages/user-info/index?username=' + username
     })
   },
-  showTypePost(){
+  showTypePost() {
     let type = this.data.postInfo.type
     wx.navigateTo({
       url: '/pages/post-type/index?type=' + type
