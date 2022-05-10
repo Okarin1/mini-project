56ad05b0ee3d714b0df66dd750b1d789bf0a2713
Retr0 <wang.zhou@hand-china.com>
2022-05-10 23:57:20 +0800

    feat: 新增回复功能

---
+++ components/send-comment/index.js
@@ -0,0 +1,48 @@
+// components/send-comment/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userInfo:{
+      type:Object,
+      value:{}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    show: false,
+    message:""
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    showPopup() {
+      let userId = this.properties.userInfo.id
+      if(userId){
+        this.setData({ show: true });
+      }else{
+        wx.showToast({
+          title: "请先登录再进行操作",
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    },
+    onClose() {
+      this.setData({ show: false });
+    },
+    sendClick: function() {
+      this.triggerEvent("click",{comment:this.data.message})
+      this.setData({
+        message:""
+      })
+      this.onClose()
+    }
+  }
+})
