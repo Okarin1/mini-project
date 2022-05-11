d9ce7866175334e76258bd161ce43f2049255f6e
Retr0 <wang.zhou@hand-china.com>
2022-05-11 23:54:53 +0800

    refactor: 重构消息通知

---
+++ store/notice-store.js
@@ -0,0 +1,39 @@
+import {
+  EventStore
+} from '../event-store/index'
+
+import {
+  getNoticeById,
+  deleteNoticeById
+} from "../../service/api_user"
+
+const userStore = new EventStore({
+      state: {
+        noticeList: {}
+      },
+      actions: {
+        getUserDataAction(ctx, username, password) {
+          getUserInfo(username, password).then(res => {
+            if(res.code == '0'){
+              ctx.userInfo = res["用户信息"]
+            }else{
+              wx.showToast({
+                title: "登录失败，用户名或密码错误",
+                icon: 'none',
+                duration: 2000
+              })
+            }
+          }).catch(err =>{
+            wx.showToast({
+              title: err,
+              icon: 'error',
+              duration: 2000
+            })
+          })
+        },
+        }
+      })
+
+    export {
+      userStore
+    }
