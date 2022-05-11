f4fb873e887574e13823f399eb3d6a5e9459f0d9
Retr0 <wang.zhou@hand-china.com>
2022-05-11 23:54:32 +0800

    feat: 新增了接口

--- service/api_home.js
+++ service/api_home.js
@@ -1,18 +1,12 @@
 import myRequest from "index"
 
 const banner = [
-  "http://p1.music.126.net/cWwMySSyjZ-N24pbHQduZg==/109951167369941967.jpg",
-  "http://p1.music.126.net/2gmbT4-zFpi_TF_nnOBjUg==/109951167368709290.jpg",
-  "http://p1.music.126.net/SmJp7vuiOILl1t8lS9o6ew==/109951167368729676.jpg",
-  "http://p1.music.126.net/_R3KoKRiGZEJpK6XuQybjw==/109951167369067602.jpg",
-  "http://p1.music.126.net/BW0x45V2G1U5eSBASR0vYA==/109951167368737211.jpg",
-  "http://p1.music.126.net/0ARJBNp78aw6ihRnsZx_Fw==/109951167368750241.jpg",
-  "http://p1.music.126.net/Xr7OLsoy2ywK3scDTVDIhg==/109951167368751495.jpg",
-  "http://p1.music.126.net/OwS8NDamftg_Zchwr3gXzA==/109951167368764654.jpg",
-  "http://p1.music.126.net/KIN2qt4H6EToFvyg-ZOGaw==/109951167368768073.jpg",
-  "http://p1.music.126.net/IYAPjMxrlavLG6R_5287Og==/109951167368776383.jpg",
-  "http://p1.music.126.net/nciCtMH66J7mmChyskCWeQ==/109951167368791525.jpg",
-  "http://p1.music.126.net/ysmb7xT6ZeaAj0ICuvUQfQ==/109951167358306463.jpg"
+  "https://s1.ax1x.com/2022/05/11/OUcAL4.png",
+  "https://s1.ax1x.com/2022/05/11/OUcAL4.png",
+  "https://s1.ax1x.com/2022/05/11/OUcAL4.png",
+  "https://s1.ax1x.com/2022/05/11/OUcAL4.png",
+  "https://s1.ax1x.com/2022/05/11/OUcAL4.png",
+  "https://s1.ax1x.com/2022/05/11/OUcAL4.png"
 ]
 export function getBanner() {
   return new Promise((resolve, reject) => {
--- service/api_phone.js
+++ service/api_phone.js
@@ -13,7 +13,14 @@ export function getRankingList() {
 }
 
 export function getPhoneInfoById(spuId) {
-  return myRequest.get("/api/product/skuinfo/attrAndGroup/1", {
-    spuId
-  })
+  return myRequest.get(`/api/product/skuinfo/info/${spuId}`,)
+}
+export function getPhoneParamById(spuId) {
+  return myRequest.get(`/api/product/skuinfo/attrAndGroup/${spuId}`)
+}
+export function getPhoneCommentList(phone_id,page,queryby = "create_date",limit = 5){
+  return myRequest.post("/api/giao/user/comment/info1",{phone_id,page,queryby,limit})
+}
+export function sendPhoneComment(phoneId,userId,userComment){
+  return myRequest.post("/api/giao/user/comment/save",{phoneId,userId,userComment})
 }
