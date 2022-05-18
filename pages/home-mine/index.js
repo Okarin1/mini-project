4a6686200cd56ad933c93833235b3ceb79bf4af6
Retr0 <wang.zhou@hand-china.com>
2022-05-18 23:39:25 +0800

    refactor: 我的页面显示关注数码

--- pages/home-mine/index.js
+++ pages/home-mine/index.js
@@ -1,117 +1,94 @@
 // pages/home-mine/index.js
-import {
-  userStore,
-} from '../../store/index'
-
-import {
-  getUserPostInfoById
-} from "../../service/api_user"
+import { userStore,followStore } from "../../store/index";
+import { getUserPostInfoById } from "../../service/api_user";
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     userInfo: {},
-    postsNum: 0
+    postsNum: 0,
+    followList:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    userStore.onState("userInfo", this.getUserHandler())
+    userStore.onState("userInfo", this.getUserHandler);
   },
 
-  getUserHandler() {
-    return (res) => {
-      this.setData({
-        userInfo: res
-      })
-      this.getMyPostsNum(res.id, 1)
+  onShow() {
+    let { id } = this.data.userInfo;
+    if(id){
+      this.getMyPostsNum(id,1)
+      followStore.dispatch("getFollowDataAction", id);
+      followStore.onState("followList",this.getFollowNum)
     }
+  },
+
+  getUserHandler(res) {
+    this.setData({
+      userInfo: res,
+    });
   },
 
   getMyPostsNum(id) {
     if (id) {
-      getUserPostInfoById(id, 1).then(res => {
+      getUserPostInfoById(id, 1).then((res) => {
         if (res.posts) {
           this.setData({
-            postsNum: res.total
-          })
+            postsNum: res.total,
+          });
+        } else {
+          this.setData({
+            postsNum: 0,
+          });
         }
-      })
+      });
     }
-
   },
+  getFollowNum(res) {
+    this.setData({
+      followList: res,
+    });
+  },
+
   handleLoginClick() {
     wx.navigateTo({
-      url: '/pages/login/index',
-    })
+      url: "/pages/login/index",
+    });
   },
+
   showMyPosts() {
-    let userInfo = JSON.stringify(this.data.userInfo)
+    let userInfo = JSON.stringify(this.data.userInfo);
     wx.navigateTo({
-      url: '/pages/my-post/index?userInfo=' + userInfo,
-    })
+      url: "/pages/my-post/index?userInfo=" + userInfo,
+    });
   },
-  removeLoginClick() {
-    userStore.setState("userInfo", {})
-    userStore.offState("userInfo", this.getUserHandler())
+  showMyFollow(){
+    wx.navigateTo({
+      url: "/pages/my-follow/index",
+    });
   },
+
   handleUserInfo() {
-    let username = this.data.userInfo.username
+    let username = this.data.userInfo.username;
     wx.navigateTo({
-      url: '/pages/user-info/index?username=' + username
-    })
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+      url: "/pages/user-info/index?username=" + username,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  removeLoginClick() {
+    userStore.setState("userInfo", {});
+    this.setData({
+      postsNum: 0,
+    });
+    followStore.setState("followList", []);
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload() {
-
+    userStore.offState("userInfo", this.getUserHandler);
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+});
