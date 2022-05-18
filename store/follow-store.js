9067490bbf61f64c46924ed9bc5138ae3151febf
Retr0 <wang.zhou@hand-china.com>
2022-05-18 23:33:59 +0800

    feat: 共享关注列表

---
+++ store/follow-store.js
@@ -0,0 +1,20 @@
+import { EventStore } from "../event-store/index";
+
+import { getFollowInfoById } from "../service/api_user";
+
+const followStore = new EventStore({
+  state: {
+    followList: [],
+  },
+  actions: {
+    getFollowDataAction(ctx, id) {
+      getFollowInfoById(id).then((res) => {
+        if (res.spuInfo) {
+          ctx.followList = res.spuInfo;
+        }
+      });
+    },
+  },
+});
+
+export { followStore };
